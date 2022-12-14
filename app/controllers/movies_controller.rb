5e51b3d90313f04289c5b2efeb18d008d7b0f500
kostik700015 <kostik700015@gmail.com>
2022-12-13 21:12:24 -0500

    released only movies querry added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ApplicationRecord
 
+  def self.released
+    where("released_on < ?", Time.now).order(released_on: :desc)
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225000000
   end

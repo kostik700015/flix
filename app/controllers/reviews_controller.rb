198332f1ee97f53c6761c21b0c6f75dd1596b6fc
kostik <kostik@new.myguest.virtualbox.org>
2023-03-28 21:13:12 -0400

    views for reviews added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,34 @@
 class ReviewsController < ApplicationController
-
+    before_action :set_movie
+    
     def index
-        @movie = Movie.find(params[:movie_id])
         @reviews = @movie.reviews
     end
+
+    def new
+        @review = @movie.reviews.new
+    end
+
+    def create
+        @review = @movie.reviews.new(review_params)
+        
+        if @review.save
+            redirect_to movie_reviews_path(@movie),
+                        notice: "Thanks for your review!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+        def set_movie
+            @movie = Movie.find(params[:movie_id])
+        end
+
+        def review_params
+            params.require(:review).permit(:name, :stars, :comment) 
+        end
+
 end

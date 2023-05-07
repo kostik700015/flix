d09c03bffede1c80b4af1f2d354f55991d3f1111
kostik <kostik@new.myguest.virtualbox.org>
2023-05-07 19:08:50 -0400

    average stars reviews added

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -10,4 +10,12 @@ module MoviesHelper
   def year_of(movie)
     movie.released_on&.year
   end
+
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, "No reviews")
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1), "star")
+    end
+  end
 end

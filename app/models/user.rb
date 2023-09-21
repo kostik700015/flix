d996b35180056437a70aa3f932cafcf2e405daec
kostik <kostik@OYE.myguest.virtualbox.org>
2023-09-20 20:50:04 -0400

    validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true 
+  validates :email, presence: true,
+            format: { with: /\S+@\S+/ },
+            uniqueness: {case_sensitive: false}
 end

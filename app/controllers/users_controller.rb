2e00db3a4e3dd9122c2455a0a247585ed3235f9e
kostik <kostik@OYE.myguest.virtualbox.org>
2023-09-21 22:49:19 -0400

    sign up created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,15 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to @user, notice: "Thanks for signing up"
+        else 
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def user_params

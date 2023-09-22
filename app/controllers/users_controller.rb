debe247b007b0281e954e2db6929b2a276042c9f
kostik <kostik@OYE.myguest.virtualbox.org>
2023-09-21 22:13:10 -0400

    view and index pages created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,11 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    private
+
+    def user_params
+        params.require(:user).
+          permit(:name, :email, :password, :password_confirmation)           
+    end
 end

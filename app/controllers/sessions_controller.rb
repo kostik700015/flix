9c84741d35c39282441d1ffdb955e80b42653516
kostik <kostik@OYE.myguest.virtualbox.org>
2023-10-01 22:41:44 -0400

    create session action added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,15 @@ class SessionsController < ApplicationController
     end
 
     def create
-        params[:email]
-        params[:password]
+       user = User.find_by(email: params[:email])
+       user.authentificate(params[:password])
+        if user && user.authentificate
+            session[:user_id] = user.id
+            redirect_to user, notice: "Wlcome back, dear #{user.name}!"
+        else
+            flash.now: "Combination email/password invalid"
+            remder :new, status: :unprocessable_entity
+        end
     end
 
     def destroy
